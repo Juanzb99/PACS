f514c727b7153c12ec5003fd6b6aa1d90028d8ca
Juan <juanzb99@gmail.com>
2022-10-30 10:34:15 +0100

    Update pi_taylor_sequential.cc

--- Laboratory-3/pi_taylor_sequential.cc
+++ Laboratory-3/pi_taylor_sequential.cc
@@ -1,15 +1,27 @@
 #include <iomanip>
+#include <chrono>
 #include <iostream>
 #include <limits>
+#include <string>
 
 // Allow to change the floating point type
 using my_float = long double;
 
 my_float pi_taylor(size_t steps) {
-    return 0.0f;
+
+    my_float pi{ 0 }; // my_float pi = 0
+    int sign = 1;
+
+    for (size_t n = 0; n < steps; n++) {
+        pi += static_cast<my_float>(sign / ((2 * n) + 1));
+        sign = -sign;
+    }
+    pi *= 4;
+
+    return pi;
 }
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[]) {
 
     // read the number of steps from the command line
     if (argc != 2) {
@@ -19,9 +31,12 @@ int main(int argc, const char *argv[]) {
     }
 
     size_t steps = std::stoll(argv[1]);
+    auto start = std::chrono::steady_clock::now();
     auto pi = pi_taylor(steps);
+    auto stop = std::chrono::steady_clock::now();
+    auto elapsed = stop - start;
 
     std::cout << "For " << steps << ", pi value: "
         << std::setprecision(std::numeric_limits<my_float>::digits10 + 1)
-        << pi << std::endl;
+        << pi << ". Elapsed time: " << elapsed.count() / 1000000000.0f << "s" << std::endl;
 }
